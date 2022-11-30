1363d16d51b01828d18462113573fcdde37bd053
rbwestphalen <rbwestphalen@gmail.com>
2022-11-29 22:42:57 -0300

    New: Transaction related components for table display

--- src/components/TransactionHistory/index.tsx
+++ src/components/TransactionHistory/index.tsx
@@ -1,6 +1,49 @@
+import { MonthTable } from "./MonthTable";
+import { TransactionTable } from "./TransactionTable";
+
+const todayString = new Date()
+	.toLocaleDateString("pt-br")
+	.split("/")
+	.reverse()
+	.join("-");
+
+type TransferDisplayFormat = [
+	created_at: string,
+	type: "in" | "out",
+	username: string,
+	amount: number
+];
+
+const mockTransfers: TransferDisplayFormat[] = [
+	["2022-11-14 15:30:00", "in", "outro_user_21", 10000],
+	["2022-11-13 15:30:00", "out", "username25", 2000],
+	["2022-11-05 15:30:00", "out", "outro_user_maior_ainda", 1723],
+	["2022-11-01 16:35:34", "out", "outro_user_menor", 1723],
+	["2022-11-01 10:11:55", "in", "brotherzito99", 1239],
+	["2022-11-01 09:42:15", "out", "Padaria Marconi", 10000],
+	["2022-10-14 15:30:00", "in", "outro_user_21", 10000],
+	["2022-10-13 15:30:00", "out", "username25", 2000],
+	["2022-10-05 15:30:00", "out", "outro_user_maior_ainda", 1723],
+	["2022-09-01 16:35:34", "out", "outro_user_menor", 1723],
+	["2022-08-01 10:11:55", "in", "brotherzito99", 1239],
+	["2022-08-01 09:42:15", "out", "Padaria Marconi", 10000],
+	["2022-07-14 15:30:00", "in", "outro_user_21", 10000],
+	["2022-07-13 15:30:00", "out", "username25", 2000],
+	["2022-07-05 15:30:00", "out", "outro_user_maior_ainda", 1723],
+	["2022-07-01 16:35:34", "out", "outro_user_menor", 1723],
+	["2022-07-01 10:07:55", "in", "brotherzito99", 1239],
+	["2022-07-01 09:42:15", "out", "Padaria Marconi", 10000],
+	["2022-06-14 15:30:00", "in", "outro_user_21", 10000],
+	["2022-06-13 15:30:00", "out", "username25", 2000],
+	["2022-06-05 15:30:00", "out", "outro_user_maior_ainda", 1723],
+	["2022-06-01 16:35:34", "out", "outro_user_menor", 1723],
+	["2022-06-01 10:06:55", "in", "brotherzito99", 1239],
+	["2022-06-01 09:42:15", "out", "Padaria Marconi", 10000],
+];
+
 export const TransactionHistory = () => {
 	return (
-		<div className='flex flex-col gap-4 px-6 py-10 bg-primary text-secondary rounded-t'>
+		<div className='flex flex-col gap-4 px-6 py-10 bg-primary text-secondary rounded-t max-h-screen'>
 			<div
 				id='filtros'
 				className='flex flex-col gap-4'>
@@ -10,7 +53,7 @@ export const TransactionHistory = () => {
 						Filtros
 					</button>
 				</div>
-				<div className='flex flex-row justify-between items-end gap-4'>
+				<div className='flex flex-row flex-wrap justify-center sm:justify-end items-end gap-4'>
 					<div className='flex gap-2'>
 						<label className='relative'>
 							<span className='absolute text-[12px] -top-2 left-2 bg-primary leading-none px-1'>
@@ -18,6 +61,8 @@ export const TransactionHistory = () => {
 							</span>
 							<input
 								type='date'
+								min={"2022-01-01"}
+								max={todayString}
 								placeholder='Data inicial'
 								className='p-1 h-8 border border-secondary bg-primary rounded'
 							/>
@@ -28,63 +73,25 @@ export const TransactionHistory = () => {
 							</span>
 							<input
 								type='date'
+								min={"2022-01-01"}
+								max={todayString}
 								placeholder='Data final'
 								className='p-1 h-8 border border-secondary bg-primary rounded'
 							/>
 						</label>
 					</div>
 					<div className='flex gap-2'>
-						<button className='w-11 h-8 font-bold rounded bg-secondary text-primary'>
+						<button className='w-16 sm:w-11 h-8 font-bold rounded bg-secondary text-primary'>
 							in
 						</button>
 
-						<button className='w-11 h-8 font-bold rounded bg-secondary text-primary'>
+						<button className='w-16 sm:w-11 h-8 font-bold rounded bg-secondary text-primary'>
 							out
 						</button>
 					</div>
 				</div>
 			</div>
-			<div
-				id='area de tabelas'
-				className='flex flex-col gap-6 border-t border-b border-black px-1 py-4'>
-				{/* mapeia registros, retorna meses */}
-				<div id='mes'>
-					<h2 className='text-2xl font-bold'>[Nome do Mês]</h2>
-					<table>
-						<tr>
-							<td>
-								{new Date().toDateString().slice(0, -5) + ". "}
-							</td>
-							<td>[seta]</td>
-							<td>[@username]</td>
-							<td>[R$ valor]</td>
-						</tr>
-					</table>
-				</div>
-				<div id='mes'>
-					<h2 className='text-2xl font-bold'>[Nome do Mês]</h2>
-					<table>
-						<tr>
-							<td>[data]</td>
-							<td>[seta]</td>
-							<td>[@username]</td>
-							<td>[R$ valor]</td>
-						</tr>
-					</table>
-				</div>
-				<div id='mes'>
-					<h2 className='text-2xl font-bold'>[Nome do Mês]</h2>
-					<table>
-						<tr>
-							<td>[data]</td>
-							<td>[seta]</td>
-							<td>[@username]</td>
-							<td>[R$ valor]</td>
-						</tr>
-					</table>
-				</div>
-				<p className='self-center'>Sem mais registros...</p>
-			</div>
+			<TransactionTable filteredTransactions={mockTransfers} />
 		</div>
 	);
 };
