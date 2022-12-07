da8f84caff0faf2b2a2c5830fb929469e9ebf422
rbwestphalen <rbwestphalen@gmail.com>
2022-12-07 10:36:38 -0300

    Backend ts setup: initial commit

---
+++ server/server.ts
@@ -0,0 +1,21 @@
+import express, { Express, Request, Response } from "express";
+import path from "path";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+app.use(express.static(path.resolve(__dirname, "frontend")));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.get("*", (req: Request, res: Response) => {
+	res.status(200).sendFile(path.resolve(__dirname, "frontend", "index.html"));
+});
+
+app.listen(PORT, () =>
+	console.log(`⚡️[server]: Server is running at https://localhost:${PORT}`)
+);
