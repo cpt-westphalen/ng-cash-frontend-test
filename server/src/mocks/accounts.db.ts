95989d10d04a1300621e933778493c43a768daf8
rbwestphalen <rbwestphalen@gmail.com>
2022-12-11 12:04:36 -0300

    Admin user mock for API testing

--- server/src/mocks/accounts.db.ts
+++ server/src/mocks/accounts.db.ts
@@ -5,4 +5,12 @@ export interface DbAccountType extends AccountType {
 	updated_at: string;
 }
 
-export const accounts_db: DbAccountType[] = [];
+export const accounts_db: DbAccountType[] = [
+	{
+		account_id: "cb5f782b-3f9b-4891-af81-5ddf7cc3e76f",
+		transaction_ids: [],
+		balance: 10000,
+		created_at: new Date().toISOString(),
+		updated_at: new Date().toISOString(),
+	},
+];
